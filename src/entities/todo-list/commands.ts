99207a51e2b7aab6a65f4e1cefd4acbf50985d8e
Stephen Caldwell <steve@campusexplorer.com>
2020-05-27 13:00:04 -0700

    streamline some code

--- src/entities/todo-list/commands.ts
+++ src/entities/todo-list/commands.ts
@@ -7,81 +7,83 @@ import {
     makeEventListItemMoved,
 } from './events';
 
+interface CommandParams {
+    userId: string;
+    list: TodoList;
+}
+
+const eventBasics = (params: CommandParams) => ({
+    userId: params.userId,
+    eventRevision: params.list.revision + 1,
+    listId: params.list.listId,
+});
+
 const createList = (params: {
     userId: string;
-    listRevision: number;
     listId: string;
     title: string;
 }) =>
     makeEventListCreated({
-        ...params,
-        eventRevision: params.listRevision + 1,
+        userId: params.userId,
+        eventRevision: 1,
+        listId: params.listId,
+        title: params.title,
     });
 
-const createListItem = (params: {
-    userId: string;
-    listRevision: number;
-    list: TodoList;
-    itemId: string;
-    text: string;
-}) => {
+const createListItem = (
+    params: CommandParams & {
+        itemId: string;
+        text: string;
+    },
+) => {
     const { list, itemId } = params;
     const currentItemIds = list.items.map((i) => i.itemId);
     if (currentItemIds.includes(itemId))
         throw new Error(`list item ${list.listId}.${itemId} already exists`);
 
     return makeEventListItemCreated({
-        userId: params.userId,
-        eventRevision: params.listRevision + 1,
-        listId: list.listId,
+        ...eventBasics(params),
         itemId,
         text: params.text,
     });
 };
 
-const completeListItem = (params: {
-    userId: string;
-    listRevision: number;
-    list: TodoList;
-    itemId: string;
-}) => {
+const completeListItem = (
+    params: CommandParams & {
+        itemId: string;
+    },
+) => {
     const { list, itemId } = params;
     const item = getItem(list, itemId);
     if (item.completed)
         throw new Error(`list item ${list.listId}.${itemId} already completed`);
     return makeEventListItemCompleted({
-        userId: params.userId,
-        eventRevision: params.listRevision + 1,
-        listId: list.listId,
+        ...eventBasics(params),
         itemId,
     });
 };
 
-const uncompleteListItem = (params: {
-    userId: string;
-    listRevision: number;
-    list: TodoList;
-    itemId: string;
-}) => {
+const uncompleteListItem = (
+    params: CommandParams & {
+        itemId: string;
+    },
+) => {
     const { list, itemId } = params;
     const item = getItem(list, itemId);
     if (!item.completed)
         throw new Error(`list item ${list.listId}.${itemId} not completed`);
     return makeEventListItemUncompleted({
-        userId: params.userId,
-        eventRevision: params.listRevision + 1,
-        listId: list.listId,
+        ...eventBasics(params),
         itemId,
     });
 };
 
-const moveListItem = (params: {
-    userId: string;
-    listRevision: number;
-    list: TodoList;
-    itemId: string;
-    newPosition: number;
-}) => {
+const moveListItem = (
+    params: CommandParams & {
+        itemId: string;
+        newPosition: number;
+    },
+) => {
     const { list, itemId, newPosition } = params;
     // asserts item existence
     getItem(list, itemId);
@@ -89,9 +91,7 @@ const moveListItem = (params: {
     if (newPosition <= 0) throw new Error('newPosition must be greater than 0');
     if (newPosition > numItems) throw new Error('newPosition is out of bounds');
     return makeEventListItemMoved({
-        userId: params.userId,
-        eventRevision: params.listRevision + 1,
-        listId: list.listId,
+        ...eventBasics(params),
         itemId,
         newPosition,
     });
