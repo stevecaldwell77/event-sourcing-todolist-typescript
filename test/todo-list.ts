a140330d6732828693380e61f54573eb0e9ea521
Stephen Caldwell <steve@campusexplorer.com>
2020-05-28 10:26:27 -0700

    more testing for todo-list

--- test/todo-list.ts
+++ test/todo-list.ts
@@ -1,41 +1,125 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import test from 'ava';
 import getId from 'src/util/get-id';
-import { makeTodoList, commands } from 'src/entities/todo-list';
-import { systemAgent } from 'src/shared/agent';
+import { makeTodoList, commands, getItem } from 'src/entities/todo-list';
+import { User } from 'src/entities/user';
+import { EntityEvent } from 'src/interfaces/entity-event';
 
-test('makeTodoList: initial', (t) => {
-    const agent = systemAgent;
+const createTestUser = (): User => ({
+    userId: getId(),
+    email: 'jdoe@gmail.com',
+    revision: 1,
+    roles: [],
+});
+
+const runSetup = () => {
+    const user = createTestUser();
     const listId = getId();
-    const todoList = makeTodoList(
-        undefined,
-        commands.createList({
-            agent,
-            listId,
-            owner: '1',
-            title: 'Test List',
+    const events: EntityEvent[] = commands.createList({
+        agent: user,
+        listId,
+        owner: user.userId,
+        title: 'My Test List',
+    });
+    const list = makeTodoList(undefined, events);
+    return { events, user, listId, list };
+};
+
+const addItem = (
+    events: EntityEvent[],
+    user: User,
+    itemText = `My Test Item ${getId()}`,
+) => {
+    const list = makeTodoList(undefined, events);
+    const itemId = getId();
+    events.push(
+        ...commands.createListItem({
+            agent: user,
+            itemId,
+            list,
+            text: itemText,
         }),
     );
+    return itemId;
+};
+
+test('createList()', (t) => {
+    const { user, listId, list } = runSetup();
     t.deepEqual(
-        todoList,
+        list,
         {
             listId,
-            owner: '1',
+            owner: user.userId,
             revision: 1,
             items: [],
-            title: 'Test List',
+            title: 'My Test List',
         },
         'initialized list looks correct',
     );
 });
 
-// create user
-// call createTodoList use case
-// call createTodoListItem use case
-// call createTodoListItem use case
-// call createTodoListItem use case
-// call completeListItem use case
-// call completeListItem use case
-// call uncompleteListItem use case
-// call getTodoList use case
-// check list
+test('createListItem()', (t) => {
+    const setup = runSetup();
+    const { user, events } = setup;
+    let { list } = setup;
+
+    const itemText = 'My Test Item';
+    const itemId = addItem(events, user, itemText);
+
+    list = makeTodoList(list, events);
+    t.deepEqual(
+        list.items,
+        [
+            {
+                itemId,
+                text: itemText,
+                completed: false,
+            },
+        ],
+        'item added to list',
+    );
+});
+
+test('item completion', (t) => {
+    const setup = runSetup();
+    const { user, events } = setup;
+    let { list } = setup;
+
+    const itemId1 = addItem(events, user);
+    const itemId2 = addItem(events, user);
+    const itemId3 = addItem(events, user);
+
+    list = makeTodoList(list, events);
+    events.push(
+        ...commands.completeListItem({
+            agent: user,
+            itemId: itemId2,
+            list,
+        }),
+    );
+
+    list = makeTodoList(list, events);
+    events.push(
+        ...commands.completeListItem({
+            agent: user,
+            itemId: itemId3,
+            list,
+        }),
+    );
+
+    list = makeTodoList(list, events);
+    t.false(getItem(list, itemId1).completed, 'item1 not completed');
+    t.true(getItem(list, itemId2).completed, 'item2 completed');
+    t.true(getItem(list, itemId3).completed, 'item3 completed');
+
+    events.push(
+        ...commands.uncompleteListItem({
+            agent: user,
+            itemId: itemId2,
+            list,
+        }),
+    );
+
+    list = makeTodoList(list, events);
+    t.false(getItem(list, itemId2).completed, 'item2 un-completed');
+});
