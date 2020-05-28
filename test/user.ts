b76ffb9a867d38ba5277cfa60f618ee86ec0fa93
Stephen Caldwell <steve@campusexplorer.com>
2020-05-28 10:01:06 -0700

    more testing for user entity

--- test/user.ts
+++ test/user.ts
@@ -3,18 +3,23 @@ import test from 'ava';
 import getId from 'src/util/get-id';
 import { makeUser, commands } from 'src/entities/user';
 import { systemAgent } from 'src/shared/agent';
+import { Role } from 'src/shared/authorization';
+import { EntityEvent } from 'src/interfaces/entity-event';
+
+const runSetup = () => {
+    const userId = getId();
+    const agent = systemAgent;
+    const events: EntityEvent[] = commands.createUser({
+        agent,
+        userId,
+        email: 'jdoe@gmail.com',
+    });
+    const user = makeUser(undefined, events);
+    return { events, agent, userId, user };
+};
 
 test('createUser()', (t) => {
-    const agent = systemAgent;
-    const userId = getId();
-    const user = makeUser(
-        undefined,
-        commands.createUser({
-            agent,
-            userId,
-            email: 'jdoe@gmail.com',
-        }),
-    );
+    const { userId, user } = runSetup();
     t.deepEqual(
         user,
         {
@@ -25,4 +30,32 @@ test('createUser()', (t) => {
         },
         'inital user created correctly',
     );
+});
+
+test('adding and removing roles', (t) => {
+    const setup = runSetup();
+    const { agent, events } = setup;
+    let { user } = setup;
+
+    events.push(
+        ...commands.addRoleToUser({
+            agent,
+            user,
+            role: Role.ADMIN,
+        }),
+    );
+
+    user = makeUser(user, events);
+    t.deepEqual(user.roles, [Role.ADMIN], 'admin role added to user');
+
+    events.push(
+        ...commands.removeRoleFromUser({
+            agent,
+            user,
+            role: Role.ADMIN,
+        }),
+    );
+
+    user = makeUser(user, events);
+    t.deepEqual(user.roles, [], 'admin role revoved from user');
 });
