46403948f344c999403e83636482805ad05b8a48
Stephen Caldwell <steve@campusexplorer.com>
2020-07-15 12:35:30 -0700

    remove io-ts

--- src/util/io-ts.ts
+++
@@ -1,9 +0,0 @@
-import { Type } from 'io-ts';
-import { isRight } from 'fp-ts/lib/Either';
-import { PathReporter } from 'io-ts/lib/PathReporter';
-
-export const coerce = <T>(schema: Type<T>) => (input: unknown): T => {
-    const validation = schema.decode(input);
-    if (isRight(validation)) return validation.right;
-    throw new Error(PathReporter.report(validation).join('\n'));
-};
